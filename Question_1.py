398aebadd5a7a51fce41ec05f750ed82d74c23ec
Jeneba-Bah <jenebagbag2006@gmail.com>
2026-04-28 14:29:54 +0000

    echo "# Assignment-One-Programming" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/Jeneba-Bah/Assignment-One-Programming.git git push -u origin main

---
+++ Question_1.py
@@ -0,0 +1,4 @@
+buyer_name = input("Kindly enter Your Name: ")
+item_purchased = input("What are you buying? :")
+
+print(f"Welcome, {buyer_name}! You have purchased {item_purchased}. ")
---
+++ Question_2.py
@@ -0,0 +1,19 @@
+def get_mark(student):
+    while True:
+        try:
+            mark = float(input(f"Enter marks for {student}: "))
+            if mark < 0 or mark > 100:
+                return mark
+            else:
+                print("error: Score must be between 0 and 100. PLEASE TRY AGAIN>")
+            except ValueError:
+print("Error: Invalid input. Please enter a numeric value.")
+Student1 = get_mark("Student 1")
+Student2 = get_mark("Student 2")
+Student3 = get_mark("Student 3")
+average = (Student1 + Student2 + Student3) / 3
+print("\n----RESULTS----")
+print(f"Student 1: {Student1}")
+print(f"Student 2: {Student2}")
+print(f"Student 3: {Student3}")
+print(f"Average Score: {average:.2f}")
